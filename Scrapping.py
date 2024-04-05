4d459b1697f07e23ba203e7a5bb135984a14260a
Tirta Rifky Fauzan <tirtarifky05@gmail.com>
2024-04-05 17:20:10 +0700

    Menambah comment

--- Scrapping.py
+++ Scrapping.py
@@ -26,98 +26,99 @@ def get_content(url):
         return None
 
 def scrape_web():
-    # Fetch the HTML content from the URL
+    # Mendapatkan content dari website republika
     html_content = get_content("https://www.republika.co.id/")
     if html_content is None:
         return None
 
-    # Parse the HTML
+    # Parsing HTML
     soup = BeautifulSoup(html_content, "html.parser")
 
-    # Create a list to store the extracted information
+    # List untuk penyimpanan data
     data = []
 
-    # Get the current system time
+    # Mendapatkan waktu sistem sekarang
     datetime_system = datetime.now().strftime("%d %B %Y %H:%M:%S")
 
-    # Find all the div elements with class "container-lit"
+    # Mencari semua div pada class container-lit
     container_lit_divs = soup.find_all("div", class_="container-lit")
 
-    # Iterate through each div with class "container-lit"
+    # Iterasi terdahap setiap div "container-lit"
     for container_lit_div in container_lit_divs:
-        # Find all the list items within the current "container-lit" div
+        # Mencari seluruh list pada class "list-group-item list-border conten1"
         list_items = container_lit_div.find_all("li", class_="list-group-item list-border conten1")
 
-        # Iterate through each list item
+        # iterasi terhadap semua list pada class "list-group-item list-border conten1"
         for list_item in list_items:
-            # Check if the list item has the class "eksternal"
+            # check apabila terdapat class "eksternal" pada list item (class ini digunakan untuk iklan)
             if "eksternal" not in list_item.get('class', []):
-                # Find the date div within the current list item
+                # Mencari div dengan class "date" pada list "list-group-item list-border conten1"
                 date_div = list_item.find("div", class_="date")
 
-                # Extract the information from the date div
-
+                # Mengambil informasi kanal (kategori) dan waktu publish dari div "date"
                 kanal_info = date_div.find("span", class_="kanal-info").text.strip()
                 time_info = date_div.text.strip().replace(kanal_info, "").strip()
 
-                # Remove the leading "- " character from time_info
+                # Menghapus karakter "- " dan spasi pada awal kalimat karena data waktu push dalam web 
+                # memiliki String "- " di awal kalimat yang tidak diperlukan
                 time_info = time_info.lstrip("- ")
 
-                # Extract the headline from the corresponding h3 tag
+                # Mendapatkan headline dari h3 pada list "list-group-item list-border conten1"
                 headline = list_item.find("h3").text.strip() if list_item.find("h3") else None
-                # Assign "None" to other values if headline is None
+                # Pengisian String apabila headline tidak ditemukan (headline kosong)
                 if headline == "":
                     headline = "SUMBER BERITA PADA POSISI INI SEDANG DOWN!!!!!!!!!!"
                     kanal_info = "DOWN"
                     time_info = "DOWN"
 
-                # Append the extracted information to the data list along with the current time
+                # Append seluruh data ke dalam list data
                 data.append({
                     "Judul": headline,
                     "Kategori": kanal_info,
                     "Waktu Publish": time_info,
                     "Waktu Scrapping": datetime_system
                 })
-
+    # Mengembalikan List data
     return data
 
-
+# Fungsi untuk menyimpan data ke dalam file JSON
 def save_to_json(data):
     if data is not None:
-        # Read existing data from JSON file
+        # Membaca data dalam file JSON
         try:
             with open("Scrapped.json", "r") as json_file:
                 json.load(json_file)
         except FileNotFoundError:
             pass
 
-        # Write the updated data to the JSON file
+        # Write data ke dalam file JSON
         with open("Scrapped.json", "w") as json_file:
             json.dump(data, json_file, indent=4)
         print("Data telah disimpan di 'Scrapped.json'.")
     else:
         print("Data tidak ditemukan.")
 
+# Fungsi untuk commit dan push perubahan ke GitHub
 def commit_and_push_changes():
     try:
-        # Add changes to the staging area
+        # Menambah perubahan ke staging area
         subprocess.run(["git", "add", "."], check=True)
         
-        # Commit the changes
+        # Commit perubahan
         subprocess.run(["git", "commit", "-m", "Update scraping data"], check=True)
         
-        # Push the changes to the remote repository
+        # Push perubahan ke remote repository
         subprocess.run(["git", "push"], check=True)
         
-        print("Changes have been committed and pushed to the remote repository.")
+        print("Commit dan Push Berhasil.")
     except subprocess.CalledProcessError as e:
         print("Error:", e)
 
-# Perform scraping
+# Pelaksanaan Scrapping
 scraped_data = scrape_web()
 
-# Save data to JSON if scraping was successful
+# Save data ke dalam file JSON
 save_to_json(scraped_data)
 
-# Commit dan push changes ke GitHub
+# Commit dan push perubahan ke GitHub
 commit_and_push_changes()
