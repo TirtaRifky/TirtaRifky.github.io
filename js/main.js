133817c57c2b57a1a767f5fddaf6244f60b4ec9e
Tirta Rifky Fauzan <tirtarifky05@gmail.com>
2024-04-13 16:55:07 +0700

    Fixing JS and HTML

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
- const mntoggle = document.querySelector('.menu-toggle input');
+const mntoggle = document.querySelector('.menu-toggle input');
  const nav = document.querySelector('nav ul');
 
 mntoggle.addEventListener('click',function(){
     nav.classList.toggle('menushow');
 })
-
 
